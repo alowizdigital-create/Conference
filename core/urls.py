a4b8d3ab880a4249a82217fabf4a9cebd3a979ac
alowizdigital-create <alowizdigital@gmail.com>
2026-07-22 07:01:31 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
-    path("", company_list, name="product_list"),
+    path("", views.dashboard, name="dashboard"),
     path("create/", company_create, name="product_create"),
     path("categories/", category_list, name="category_list"),
     path("categories/create/", category_create, name="category_create"),
-
+  
 ]
