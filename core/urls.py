43de7b1910138902633f2318de63b56d01fffec4
alowizdigital-create <alowizdigital@gmail.com>
2026-07-22 09:39:54 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,6 @@ from .views import *
 # from . import views
 
 urlpatterns = [
-    path("dashboard/", views.dashboard, name="dashboard"),
     path("create/", company_create, name="product_create"),
     path("categories/", category_list, name="category_list"),
     path("categories/create/", category_create, name="category_create"),
