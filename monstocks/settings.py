7c833c5ae0631218cbd455383baa054e69fa77d3
alowizdigital-create <alowizdigital@gmail.com>
2026-07-22 13:20:08 +0200

    Update settings.py

--- monstocks/settings.py
+++ monstocks/settings.py
@@ -114,28 +114,28 @@ WSGI_APPLICATION = 'monstocks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('DB_NAME', 'zweeybd'),
-#         'USER': os.getenv('DB_USER', 'zweeyuser'),
-#         'PASSWORD': os.getenv('DB_PASSWORD', 'Jeanpierre236'),
-#         'HOST': os.getenv('DB_HOST', 'stocks-zweey-uhezrn'),
-#         'PORT': os.getenv('DB_PORT', '3306'),
-#         'OPTIONS': {
-#             'charset': 'utf8mb4',
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'conferencedb'),
+        'USER': os.getenv('DB_USER', 'conferenceuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Jeanpierre236'),
+        'HOST': os.getenv('DB_HOST', 'conference-conferencedb-yly2qq'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
 
 LOGIN_URL = 'connexion'
 
